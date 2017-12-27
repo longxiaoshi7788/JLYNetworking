b12f6fbdffa98cd91e9fda9a08125d54ef057485
July <shixiaolong@51haoyayi.com>
2017-12-27 08:27:58 +0800

    ignore pods *

---
+++ JLYNetworking/JLYNetworking/JLYHTTPSessionManager.h
@@ -0,0 +1,13 @@
+//
+//  JLYHTTPSessionManager.h
+//  JLYNetworking
+//
+//  Created by Allen on 2017/12/26.
+//  Copyright © 2017年 jly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JLYHTTPSessionManager : NSObject
+
+@end
---
+++ JLYNetworking/JLYNetworking/JLYHTTPSessionManager.m
@@ -0,0 +1,13 @@
+//
+//  JLYHTTPSessionManager.m
+//  JLYNetworking
+//
+//  Created by Allen on 2017/12/26.
+//  Copyright © 2017年 jly. All rights reserved.
+//
+
+#import "JLYHTTPSessionManager.h"
+
+@implementation JLYHTTPSessionManager
+
+@end
